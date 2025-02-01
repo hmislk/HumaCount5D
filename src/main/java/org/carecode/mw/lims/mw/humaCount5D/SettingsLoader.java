fd5ce0d6bee64b4a8d93c8fb33c66e570bfcde28
DamithDeshan <hkddrajapaksha@gmail.com>
2025-02-01 22:19:45 +0530

    Signed-off-by: DamithDeshan <hkddrajapaksha@gmail.com>

--- src/main/java/org/carecode/mw/lims/mw/humaCount5D/SettingsLoader.java
+++ src/main/java/org/carecode/mw/lims/mw/humaCount5D/SettingsLoader.java
@@ -8,7 +8,7 @@ import java.nio.file.Paths;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.carecode.lims.libraries.MiddlewareSettings;
-
+ 
 public class SettingsLoader {
 
     private static final Logger logger = LogManager.getLogger(SettingsLoader.class);
